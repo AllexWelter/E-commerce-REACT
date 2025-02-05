69efd9b184b231ee5638841bbfcc534cafcd2c89
Allex Welter <allexwelter13@gmail.com>
2025-02-05 08:55:35 -0300

    .

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -15,7 +15,7 @@ const {contadorCarrinho}= useContext(CarrinhoContext)
                 <Container>
                     <Navbar.Brand href="#home">E-Commerce</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="/home">Home</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/produtos">Produtos</Nav.Link>
                     </Nav>
                     <Navbar.Collapse className="justify-content-end">
